acd3d0aae0d7406f17bca86a32c2c1cc1222c9df
Filip Stojanovic <mercurial.the.spectre@gmail.com>
2024-09-03 15:19:59 +0200

    useParams and refactor with reactQuery

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 // import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 // import Pet from "./Pet";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   // return React.createElement(
@@ -28,11 +38,15 @@ const App = () => {
   // );
   return (
     <BrowserRouter>
-      <h1>Adopt Me!</h1>
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
